1d3151a19dea3e9bbfbb9b5163c9c167d49813c0
Miguel Lima <miguelimajr@gmail.com>
2021-09-25 20:41:46 -0300

    ajustes

--- SGP/js/script.js
+++ SGP/js/script.js
@@ -1,3 +1,35 @@
 function salvar() {
-    console.log ("OK")
+
+    nome = document.getElementById("nome").value 
+    var matricula = document.getElementById("matricula").value 
+    var idFuncional = document.getElementById("idFuncional").value 
+    var cargo = document.getElementById("cargo").value 
+    var lotacao = document.getElementById("lotacao").value 
+    var cpf = document.getElementById("cpf").value 
+    var nascimento = document.getElementById("nascimento").value 
+    var tel = document.getElementById("tel").value 
+    var cel = document.getElementById("cel").value 
+    var email = document.getElementById("email").value  
+    
+    
+        
+    console.log (nome)
+
+    document.getElementById("inputNome").innerHTML = nome
+    
+
+
+
+}
+
+
+function reset () {
+
+    /* var nome = document.getElementById("nome") */
+    console.log (nome)
+    
+    /* nome = "teste" */
+    document.getElementById("inputNome").innerHTML = "<input type='text' id='nome' class='inputDuplo' value= '"+nome +"'/>"
+
+    
 }
